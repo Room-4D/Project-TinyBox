6f9634f33af667d9568290163dc65ad4117b0f38
Nathan Marcellous <taboodog@hotmail.com>
2025-01-04 19:49:01 -0800

    removed name

--- src/lib/data/schema.ts
+++ src/lib/data/schema.ts
@@ -3,7 +3,6 @@ import type { InferSelectModel } from 'drizzle-orm';
 
 export const userTable = pgTable('user', {
   id: serial('id').primaryKey(),
-  name: text('name').notNull(),
 });
 
 export const sessionTable = pgTable('session', {
