21c4792eb4ab576bf543dcdc18d63348d318cfab
Korey Luu <koreyluu@gmail.com>
2025-01-08 18:18:44 -0800

    change redirect route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation';
 export default async function Home() {
   const { user } = await getCurrentSession();
   if (user === null) {
-    return redirect('/login');
+    return redirect('/auth/login');
   }
 
   return <div>Hello World</div>;
